822a80e39fc65535edd1aaec869d48ab71af703b
Jose Cerezo <jcerezorios@gmail.com>
2023-09-23 09:54:18 -0400

    Mostrar mejorado

--- php/CRUD_CATEGORIA/mostrar_CATEGORIA.php
+++ php/CRUD_CATEGORIA/mostrar_CATEGORIA.php
@@ -1,15 +1,39 @@
-<?php
-$mysqli_link = mysqli_connect("localhost", "root", "", "negocioxy");
-if (mysqli_connect_errno()) {
-    printf("MySQL connection failed with the error: %s", mysqli_connect_error());
-    exit;
-}
-$select_query = "SELECT * FROM categoria LIMIT 10";
-$result = mysqli_query($mysqli_link, $select_query);
-while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-    echo "Nombre:" . $row['Nombre'] . "<br/>";
-    echo "Descripcion:" . $row['Descripcion'] . "<br/>";
-    echo "<br/>";
-}
-// close the db connection
-mysqli_close($mysqli_link);
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabla de Proveedores</title>
+    <link rel="stylesheet" href="../../css/mostrarEstilo.css">
+</head>
+<body>
+    <h1>Clientes</h1>
+    <?php
+    $mysqli_link = mysqli_connect("localhost", "root", "", "negocioxy");
+    if (mysqli_connect_errno()) {
+        printf("MySQL connection failed with the error: %s", mysqli_connect_error());
+        exit;
+    }
+    $select_query = "SELECT * FROM categoria LIMIT 10";
+    $result = mysqli_query($mysqli_link, $select_query);
+    ?>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Descripcion</th>
+        </tr>
+        <?php
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            echo "<tr>";
+            echo "<td>" . $row['Id'] . "</td>";
+            echo "<td>" . $row['Nombre'] . "</td>";
+            echo "<td>" . $row['Descripcion'] ."</td>";
+            echo "</tr>";
+        }
+        // close the db connection
+        mysqli_close($mysqli_link);
+        ?>
+    </table>
+</body>
+</html>
--- php/CRUD_PROVEEDOR/mostrar_PROVEEDOR.php
+++ php/CRUD_PROVEEDOR/mostrar_PROVEEDOR.php
@@ -1,17 +1,43 @@
-<?php
-$mysqli_link = mysqli_connect("localhost", "root", "", "negocioxy");
-if (mysqli_connect_errno()) {
-    printf("MySQL connection failed with the error: %s", mysqli_connect_error());
-    exit;
-}
-$select_query = "SELECT * FROM proveedor LIMIT 10";
-$result = mysqli_query($mysqli_link, $select_query);
-while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-    echo "Nombre:" . $row['Nombre'] . "<br/>";
-    echo "Direccion:" . $row['Direccion'] . "<br/>";
-    echo "Telefono:" . $row['Telefono'] . "<br/>";
-    echo "Web:" . $row['Web'] . "<br/>";
-    echo "<br/>";
-}
-// close the db connection
-mysqli_close($mysqli_link);
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabla de Proveedores</title>
+    <link rel="stylesheet" href="../../css/mostrarEstilo.css">
+</head>
+<body>
+    <h1>Proveedores</h1>
+    <?php
+    $mysqli_link = mysqli_connect("localhost", "root", "", "negocioxy");
+    if (mysqli_connect_errno()) {
+        printf("MySQL connection failed with the error: %s", mysqli_connect_error());
+        exit;
+    }
+    $select_query = "SELECT * FROM proveedor LIMIT 10";
+    $result = mysqli_query($mysqli_link, $select_query);
+    ?>
+    <table>
+        <tr>
+            <th>RUT</th>
+            <th>Nombre</th>
+            <th>Dirección</th>
+            <th>Teléfono</th>
+            <th>Web</th>
+        </tr>
+        <?php
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            echo "<tr>";
+            echo "<td>" . $row['RUT'] . "</td>";
+            echo "<td>" . $row['Nombre'] . "</td>";
+            echo "<td>" . $row['Direccion'] . "</td>";
+            echo "<td>" . $row['Telefono'] . "</td>";
+            echo "<td><a href='" . $row['Web'] . "' target='_blank'>" . $row['Web'] . "</a></td>";
+            echo "</tr>";
+        }
+        // close the db connection
+        mysqli_close($mysqli_link);
+        ?>
+    </table>
+</body>
+</html>
